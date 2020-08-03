d1348a0b5e796c885f0271685b55692a89f8b615
admin <admin@DESKTOP-K1K0Q69>
2020-08-03 11:12:30 +0900

    Initial commit

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,23 @@
+/*
+ * DATE : 2020년 6월 26일
+ * Author : 홍길동
+ * 설명 : 첫 번째 자바 프로그램입니다.
+ * Version : 1.0
+ */
+
+package hello;
+
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		//주석입니다. 코드를 설명, 개발자가 보기 위함
+		System.out.println("한은영");
+		/*
+		 이것은
+		 여러줄 
+		  주석		 
+		 */
+	
+	}
+
+}
